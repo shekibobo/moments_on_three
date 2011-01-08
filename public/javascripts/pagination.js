91fe03e0c9df5c260ec4f82c73ea0995103badcf
Joshua Kovach <kovach.jc@gmail.com>
2011-01-08 13:26:28 -0500

    updated the gallery pages to use ajax history through jquery bbq plugin

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,7 +1,24 @@
+/*
+ *The following block of code was adapted from https://gist.github.com/358429
+ * by ewall as an interpretation of Railscast 175 replacing url_utils with bbq
+ */
+base_url = location.protocol + '//' + location.host + location.pathname;
 $(function() {
-  $(".pagination a").live('click', function() {
+  $(".pagination a").live('click', function(e) {
+    params = $.deparam.querystring(this.href);
+    new_url = $.param.fragment(base_url, params, 2);
     $(".pagination").html("Loading photos...");
-    $.getScript(this.href);
+    location.hash = $.param.fragment( new_url );
+    // $.getScript(this.href);
     return false;
   });
 });
+
+$(window).bind('hashchange', function(e) {
+  params = e.fragment;
+  $.getScript($.param.querystring(base_url, params) );
+});
+
+if (location.hash) {
+  $(window).trigger( 'hashchange' );
+}
