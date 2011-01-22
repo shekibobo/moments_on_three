1249cae7f1b8f0da254afe8d2a95a00abc9a61bc
Joshua Kovach <kovach.jc@gmail.com>
2011-01-22 12:55:09 -0500

    fixed gallery preview issue for gallery#index

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -5,6 +5,8 @@ class Gallery < ActiveRecord::Base
   attr_accessible :name, :owner_id, :shoot_date, :description,
     :category_id, :archive
 
+  
+
   has_attached_file :archive,
     :path => ":rails_root/public/:attachment/:basename.:extension"
   validates_attachment_content_type :archive, :content_type => 'application/zip'
