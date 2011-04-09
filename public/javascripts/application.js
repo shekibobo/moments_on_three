a1c951e7498553e5e5eb4a8b8679943dc48a2dfa
Joshua Kovach <kovach.jc@gmail.com>
2011-04-09 03:17:14 -0400

    fixed button positioning in photo galleries; fadeToggle works great, but currently incompatible with ajax pagination in its current state.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
+
+  /*
+   * This hides the control buttons unless the specific photo is hovered over.
+   */
   $('.photo .control').hide();
   $('.photo').hover(function() {
     $(this).contents('.control').fadeToggle();
