49124055fd9960a06a7dff18e51c1ffeebd75f3e
Joshua Kovach <kovach.jc@gmail.com>
2011-04-11 00:07:37 -0400

    added accessibles to orders controller and changed flash messages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_filter :authenticate_user!
-  
+
   def index
     if admin_signed_in?
       @orders_in_progress = Order.find_by_committed(false)
@@ -15,6 +15,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
 
   def new
@@ -25,7 +26,7 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(params[:order])
     if @order.save
-      flash[:notice] = "Successfully created order."
+      flash[:notice] = "Order created."
       redirect_to @order
     else
       render :action => 'new'
@@ -49,7 +50,8 @@ class OrdersController < ApplicationController
   def destroy
     @order = Order.find(params[:id])
     @order.destroy
-    flash[:notice] = "Successfully destroyed order."
+    flash[:notice] = "Order canceled."
     redirect_to orders_url
   end
+  
 end
