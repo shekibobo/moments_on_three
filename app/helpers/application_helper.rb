3b8de01fa5bc6ee90bde20f41ac1ac0500b9d2bf
Joshua Kovach <kovach.jc@gmail.com>
2011-04-11 17:44:45 -0400

    rewrote my edit and delete button api and updated all calls to allow more dynamic functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,32 +1,33 @@
 module ApplicationHelper
-  def edit_button_for(*object)
-    link_to button(:edit), [ :edit ] + object
+  def edit_button_for(arr, options={})
+    link_to button(:edit, options[:alt]), [ :edit ] + arr
   end
 
-  def delete_button_for(*object, options)
-    link_to button(:delete), object,
+  def delete_button_for(arr, options={})
+    options[:confirm] ||= "Are you sure?"
+    link_to button(:delete, options[:alt]), arr,
       :confirm => options[:confirm], :method => :delete
   end
   alias destroy_button_for delete_button_for
 
-  def like_button_for(*object)
-    link_to button(:thumb_up), ""
+  def like_button_for(arr, options={})
+    link_to button(:thumb_up, options[:alt]), ""
   end
 
-  def new_button_for(*object)
-    link_to button(:add), [:new] + object
+  def new_button_for(arr, options={})
+    link_to button(:add, options[:alt]), [:new] + arr
   end
 
   def button(type, alt="")
     image_tag "icons/#{type.to_s}.png", :class => 'button', :alt => alt
   end
 
-  def add_cart_button_for(object, attr_hash={})
-    link_to button(:cart_add), polymorphic_url( [:new] + object, attr_hash )
+  def add_cart_button_for(arr, attr_hash={})
+    link_to button(:cart_add), polymorphic_url( [:new] + arr, attr_hash )
   end
 
-  def remove_cart_button_for(*object, options)
-    link_to button(:cart_delete), object,
+  def remove_cart_button_for(arr, options={})
+    link_to button(:cart_delete), arr,
       :confirm => options[:confirm], :method => :delete
   end
 end
