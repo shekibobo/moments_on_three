fa5e89e3b8db4a05c873ec2001780eff6d487b57
Joshua Kovach <kovach.jc@gmail.com>
2011-04-13 21:40:23 -0400

    enabled text display in button helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def edit_button_for(arr, options={})
-    link_to button(:edit, options[:alt]), [ :edit ] + arr
+    link_to button(:edit, options), [ :edit ] + arr
   end
 
   def delete_button_for(arr, options={})
     options[:confirm] ||= "Are you sure?"
-    link_to button(:delete, options[:alt]), arr,
+    link_to button(:delete, options), arr,
       :confirm => options[:confirm],
       :method => :delete,
       :class => "delete",
@@ -14,15 +14,16 @@ module ApplicationHelper
   alias destroy_button_for delete_button_for
 
   def like_button_for(arr, options={})
-    link_to button(:thumb_up, options[:alt]), ""
+    link_to button(:thumb_up, options), ""
   end
 
   def new_button_for(arr, options={})
-    link_to button(:add, options[:alt]), [:new] + arr
+    link_to button(:add, options), [:new] + arr
   end
 
-  def button(type, alt="")
-    image_tag "icons/#{type.to_s}.png", :class => 'button', :alt => alt
+  def button(type, options={:alt => ""})
+    image_tag("icons/#{type.to_s}.png", :alt => options[:alt], :class => 'button') +
+      ( options[:text] ? content_tag(:span, :class => 'button') { options[:alt] } : "" )
   end
 
   def add_cart_button_for(arr, attr_hash={})
