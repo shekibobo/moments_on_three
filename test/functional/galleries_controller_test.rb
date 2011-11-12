cca4c34045e4f8eafd5fdd299f8366da6bb40d72
Joshua Kovach <kovach.jc@gmail.com>
2011-11-12 15:06:24 -0500

    Fix a lot of errors in tests

--- test/functional/galleries_controller_test.rb
+++ test/functional/galleries_controller_test.rb
@@ -1,51 +1,66 @@
 require 'test_helper'
 
 class GalleriesControllerTest < ActionController::TestCase
+  def setup
+    @admin = users(:eryn)
+    @owner = users(:molly)
+    @shared_user = users(:david)
+    @unshared_user = users(:owen)
+  end
+
   def test_index
     get :index
     assert_template 'index'
   end
-  
-  def test_show
+
+  def test_show_for_admin
+    sign_in @admin
     get :show, :id => Gallery.first
     assert_template 'show'
   end
-  
-  def test_new
+
+  def test_new_for_admin
+    sign_in @admin
     get :new
     assert_template 'new'
   end
-  
-  def test_create_invalid
+
+  def test_create_invalid_for_admin
+    sign_in @admin
     Gallery.any_instance.stubs(:valid?).returns(false)
     post :create
     assert_template 'new'
   end
 
-  def test_create_valid
+  def test_create_valid_for_admin
+    sign_in @admin
     Gallery.any_instance.stubs(:valid?).returns(true)
     post :create
     assert_redirected_to gallery_url(assigns(:gallery))
   end
-  
-  def test_edit
+
+  def test_edit_for_admin
+    sign_in @admin
     get :edit, :id => Gallery.first
     assert_template 'edit'
   end
-  
-  def test_update_invalid
+
+  def test_update_invalid_for_admin
+    sign_in @admin
     Gallery.any_instance.stubs(:valid?).returns(false)
     put :update, :id => Gallery.first
     assert_template 'edit'
   end
 
-  def test_update_valid
+  def test_update_valid_for_admin
+    sign_in @admin
     Gallery.any_instance.stubs(:valid?).returns(true)
     put :update, :id => Gallery.first
     assert_redirected_to gallery_url(assigns(:gallery))
   end
-  
-  def test_destroy
+
+  def test_destroy_for_admin
+    sign_in @admin
     gallery = Gallery.first
     delete :destroy, :id => gallery
     assert_redirected_to galleries_url
--- test/functional/permissions_controller_test.rb
+++ test/functional/permissions_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class PermissionsControllerTest < ActionController::TestCase
+  def setup
+    @senior_gallery = galleries(:senior_gallery)
+    Gallery.stubs(:find).returns(@senior_gallery)
+    sign_in users(:molly)
+  end
+
   def test_new
     get :new
     assert_template 'new'
@@ -19,24 +25,24 @@ class PermissionsControllerTest < ActionController::TestCase
   end
 
   def test_edit
-    get :edit, :id => Permission.first
+    get :edit, :id => @senior_gallery.permissions.first
     assert_template 'edit'
   end
 
   def test_update_invalid
     Permission.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => Permission.first
+    put :update, :gallery_id => @senior_gallery.id, :id => @senior_gallery.permissions.first
     assert_template 'edit'
   end
 
   def test_update_valid
     Permission.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => Permission.first
+    put :update, :id => @senior_gallery.permissions.first
     assert_redirected_to root_url
   end
 
   def test_destroy
-    permission = Permission.first
+    permission = @senior_gallery.permissions.first
     delete :destroy, :id => permission
     assert_redirected_to root_url
     assert !Permission.exists?(permission.id)
