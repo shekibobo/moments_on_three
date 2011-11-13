f84529dc7fb1b59f779d5ba0b97b534f764ff0e2
Joshua Kovach <kovach.jc@gmail.com>
2011-11-13 09:52:52 -0500

    Fix failing order save

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,17 @@
 class Order < ActiveRecord::Base
   attr_accessible :user_id, :committed, :paid, :subtotal, :total
 
-  before_save :subtotal=
-  before_save :total=
+  before_save :set_subtotal
+  before_save :set_total
 
   belongs_to :user
   has_many :order_items, :dependent => :destroy
 
-  def subtotal=()
+  def set_subtotal
     self[:subtotal] = order_items.collect{|i| i.subtotal}.inject(:+) || 0
   end
 
-  def total=()
+  def set_total
     tax = 0.06
     self[:total] = subtotal * (1 + tax)
   end
